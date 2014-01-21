20c5bfdaccc029c0e00afbbe5666a2c915b5ff55
Georg Heesch <georg@evil-lair.org>
2014-01-21 14:02:01 +0100

    changed barchart to have client names as ticks

--- stats/renderer/fails_per_host.sh
+++ stats/renderer/fails_per_host.sh
@@ -18,13 +18,14 @@ n=`head -n2 $tmpfile | tail -n1 | wc -w | cut -d "	" -f2`
 
 (echo -n "set terminal svg size 1200,600 fname 'Verdana' fsize 10
 set style fill   solid 1.00 border lt -1
+set xtics border in scale 1,0.5 nomirror rotate by -45 offset character 0, 0, 0 
 set datafile missing '-'
 set style data histogram
 set style histogram
 set style fill solid border -1
 set boxwidth 0.75
 set key outside;
-plot [-1:] \"$tmpfile\" using 4 title \"fails per host\""
+plot [-1:] \"$tmpfile\" using 4:xticlabels(1) notitle"
 echo
 ) | gnuplot
 rm $tmpfile
