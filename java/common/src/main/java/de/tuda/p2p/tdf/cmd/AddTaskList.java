fd625820fe4cae32a05381f1499e5dfa32283801
Georg Heesch <georg@evil-lair.org>
2014-01-28 14:45:36 +0100

    debug

--- java/common/src/main/java/de/tuda/p2p/tdf/cmd/AddTaskList.java
+++ java/common/src/main/java/de/tuda/p2p/tdf/cmd/AddTaskList.java
@@ -14,21 +14,20 @@ public class AddTaskList extends CMD{
 	
 
 	private static String addTaskList(JsonNode jn){
-
-		System.out.println(jn.hasFields());
-		
 		if (!jn.hasFields()) return null;
 		TaskList t = new TaskList();
 		RedisHash rh = new RedisHash();
 		for (JsonField i : jn.getFieldList()) {
 			if (i.getName().getText().matches("(?i)ID")) {
 				t.setIndex(Long.valueOf(i.getValue().getText()));
-				;
+				System.out.println("index");
 			} else if (i.getName().getText().matches("(?i)Namespace")) {
 				t.setNamespace(i.getValue().getText());
+				System.out.println("namespace");
 			} else if (i.getName().getText().matches("(?i)Namespace")) {
 				for (JsonNode task : i.getValue().getElements())
 					t.addtask(addTask(task));
+				System.out.println("tasks: "+i.getValue().getElements().size());
 				break;
 			} else {
 				Object v = null;
