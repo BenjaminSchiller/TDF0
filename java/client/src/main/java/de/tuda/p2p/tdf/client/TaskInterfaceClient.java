393e8bab095bf2fdc50730e19980021348029601
Georg Heesch <georg@evil-lair.org>
2014-01-29 17:25:18 +0100

    debug

--- java/client/src/main/java/de/tuda/p2p/tdf/client/TaskInterfaceClient.java
+++ java/client/src/main/java/de/tuda/p2p/tdf/client/TaskInterfaceClient.java
@@ -81,6 +81,7 @@ public class TaskInterfaceClient {
 	 * @return The Jedis object
 	 */
 	public Jedis getJedis() {
+		Client.logMessage(jedis.info());
 		Integer i = 0;
 		while ( ! jedis.isConnected() || i < 10) {
 			i++;
