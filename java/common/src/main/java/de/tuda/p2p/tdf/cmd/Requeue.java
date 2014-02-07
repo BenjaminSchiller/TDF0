bdef851b71b3dfbe5b531f3e766d30e1958459e5
Georg Heesch <georg@evil-lair.org>
2014-02-07 11:27:16 +0100

    requeue debug

--- java/common/src/main/java/de/tuda/p2p/tdf/cmd/Requeue.java
+++ java/common/src/main/java/de/tuda/p2p/tdf/cmd/Requeue.java
@@ -87,7 +87,7 @@ public class Requeue extends CMD {
 		say("tasks:"+tasks.size());
 		java.util.Collections.sort(tasks, new TaskComparator());
 		say("tasks:"+tasks.size());
-		int size = evenout?(int) Math.ceil(tasks.size()/(Math.ceil(tasks.size()/listsize))):listsize;
+		int size = evenout?(int) Math.floor(tasks.size()/(Math.ceil(tasks.size()/listsize))):listsize;
 		for (Task task : tasks	) requeue(task,size);
 		
 		return ;
