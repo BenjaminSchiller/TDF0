94815753e2ff4e0febf28d537395a8fbafa42504
Georg Heesch <georg@evil-lair.org>
2014-02-15 09:05:01 +0100

    added capability to use differing src and target dirs

--- java/create-jars.sh
+++ java/create-jars.sh
@@ -2,7 +2,8 @@
 
 tmpdir=`mktemp -d`
 manifestfile=`mktemp`
-srcdir=`pwd`
+srcdir=${1:-`pwd`}
+targetdir=${2:-`pwd`}
 trap "rm -rf $manifestfile $tmpdir" EXIT
 jars="argo-3.7.jar commons-cli-20040117.000000.jar commons-io-2.4.jar gson-2.2.4.jar hamcrest-all-1.3.jar jedis-1.5.2.jar joda-time-2.3.jar json-20090211.jar junit-4.11.jar zip4j_1.3.1.jar"
 actions="AddNamespace AddTask DeleteNamespace DeleteTask DeleteTaskList ExportProcessed Requeue Show"
@@ -18,17 +19,16 @@ rm -r META-INF LICENSE.txt
 
 for action in $actions; do
  echo -e $manifestheader de.tuda.p2p.tdf.cmd.$action > $manifestfile
- jar cfm $srcdir/$action.jar $manifestfile *
+ jar cfm $targetdir/$action.jar $manifestfile *
 done
 
 rm -r $tmpdir/de/tuda/p2p/tdf/cmd
 cp -r $srcdir/client/bin/de $tmpdir
 
-echo -e $manifestheader de.tuda.p2p.tdf.cmd.Client > $manifes
-tfile
-jar cfm $srcdir/client.jar $manifestfile *
+echo -e $manifestheader de.tuda.p2p.tdf.cmd.Client > $manifestfile
+jar cfm $targetdir/client.jar $manifestfile *
 
 cd $srcdir/logserver/bin/
 echo -e $manifestheader logserver.logserver > $manifestfile
-jar cfm $srcdir/logserver.jar $manifestfile *
+jar cfm $targetdir/logserver.jar $manifestfile *
 
