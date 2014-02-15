45f35d1916d1b8d5a4d12f16cb25968bcec2755c
Georg Heesch <georg@evil-lair.org>
2014-02-15 15:38:34 +0100

    fixed deletenamespace

--- java/common/src/main/java/de/tuda/p2p/tdf/cmd/DeleteNamespace.java
+++ java/common/src/main/java/de/tuda/p2p/tdf/cmd/DeleteNamespace.java
@@ -17,21 +17,18 @@ public class DeleteNamespace extends CMD {
 	
 	public static boolean deleteNamespace(String namespace){
 		
-		for (String i : jedis.keys("tdf." + namespace + ".task.*")) 
-			DeleteTask.deleteTask("tdf." + namespace + ".task." + i);
-		for (String i : jedis.keys("tdf." + namespace + ".tasklist.*")) 
-			DeleteTaskList.deleteTaskList("tdf." + namespace + ".tasklist." + i);
-		
-		
 		// keys to be deleted from database, they are going to be prefixed with
 		// '"tdf." + namespace + "."'
 		List<String> keys = new ArrayList<String>();
-		keys.add("tdf." + namespace + "."+"queuing");
-		keys.add("tdf." + namespace + "."+"running");
-		keys.add("tdf." + namespace + "."+"completed");
-		keys.add("tdf." + namespace + "."+"processed");
-		keys.add("tdf." + namespace + "."+"index");
-		keys.add("tdf." + namespace + "."+"new");
+//		keys.add("tdf." + namespace + "."+"queuing");
+//		keys.add("tdf." + namespace + "."+"running");
+//		keys.add("tdf." + namespace + "."+"completed");
+//		keys.add("tdf." + namespace + "."+"processed");
+//		keys.add("tdf." + namespace + "."+"index");
+//		keys.add("tdf." + namespace + "."+"new");
+//		keys.addAll(jedis.keys("tdf." + namespace + ".task.*"));
+//		keys.addAll(jedis.keys("tdf." + namespace + ".tasklist.*"));
+		keys.addAll(jedis.keys("tdf." + namespace + "*"));
 		jedis.del(keys.toArray(new String[0]));
 		
 		jedis.srem("tdf.namespaces",namespace);
