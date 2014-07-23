c5a6df6012750679b8057582e46d3e7d2a141e53
jan <gitdev@privat.doenerbier.de>
2014-07-23 15:40:03 +0200

    Fixed commands sh; Delete Tasks now with whole namepace support

--- java/common/commands.sh
+++ java/common/commands.sh
@@ -4,4 +4,4 @@ pwd=$(dirname $0)
 
 echo $pwd
 
-java -cp ${pwd}/commands.jar de.tuda.p2p.tdf.cmd.$(basename $0)
+java -cp ${pwd}/commands.jar de.tuda.p2p.tdf.cmd.$(basename $0) $@
--- java/common/src/main/java/de/tuda/p2p/tdf/cmd/DeleteTask.java
+++ java/common/src/main/java/de/tuda/p2p/tdf/cmd/DeleteTask.java
@@ -1,11 +1,49 @@
 package de.tuda.p2p.tdf.cmd;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import argo.jdom.JsonNode;
 
 public class DeleteTask extends CMD {
 	
 	public static void main(String[] args){
-		delete(parsejson(getInput(args)));
+		init();		
+		if(args.length == 2 && args[0].equals("-a")) {
+			System.out.println("Deleting all tasks in namespace " + args[1]);
+			deleteTasksNamespace(args[1]);
+		}
+		else {
+			delete(parsejson(getInput(args)));
+		}
+	}
+	
+	public static boolean deleteTasksNamespace(String namespace)
+	{
+		Set<String> tasksSet = new HashSet<String>();
+		Set<String> taskListsSet = new HashSet<String>();
+
+		List<String> tasks = jedis.lrange("tdf." + namespace + ".queuing", 0, jedis.llen("tdf." + namespace + ".queuing"));
+		
+		if(tasks != null)
+			tasksSet.addAll(tasks);
+		
+		List<String> taskLists = jedis.lrange("tdf." + namespace + ".queuing", 0, jedis.llen("tdf." + namespace + ".queuinglists"));
+		if(taskLists != null)
+			taskListsSet.addAll(taskLists);
+		
+		for(String task : tasksSet){
+			jedis.del("tdf." + namespace + ".task." + task);
+		}
+		
+		for(String taskList : taskListsSet){
+			jedis.del("tdf." + namespace + ".tasklist." + taskList + ".tasks");
+		}
+		
+		jedis.del("tdf." + namespace + ".queuing", "tdf." + namespace + ".queuinglists", "tdf." + namespace + ".running", "tdf." + namespace + ".completed", "tdf." + namespace + ".processed");
+				
+		return true;
 	}
 
 	public static boolean deleteTask(String identifier) {
