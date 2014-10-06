51d84ddfd382098d02f26db8aab6f1eea7a78194
jan <gitdev@privat.doenerbier.de>
2014-10-06 13:50:31 +0200

    Removed unused stuff, corrected AddTask to have a integer model

--- java/stock-worker/src/main/resources/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Kill the spawned Python process if the run script is aborted
-trap "kill 0" SIGINT SIGTERM EXIT
-
-# Start the worker and hand over the command line parameters
-python worker.py $1 $2
